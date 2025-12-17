029c971de1b2dc5837dbaeb555c11a004bbb05e4
Andrii Stoliarov <andrey159852@gmail.com>
2025-12-17 19:40:22 +0200

    lesson 11 passed

--- angular/yuriSimonov/basic-course-2023/src/app/app.component.ts
+++ angular/yuriSimonov/basic-course-2023/src/app/app.component.ts
@@ -10,52 +10,44 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'basic course 2023';
-  parentName: string = 'hello, world!';
-
-  user = {
-    name: 'Yura',
-    age: '27',
-  };
-
-  tooltip = 'Я подсказка для ссылки';
-
-  inLineStyle = {
-    width: '50%',
-    background: 'green',
-  };
-
-  cssClass = 'blue';
-
-  someNumber = 50;
-
-  someDate = new Date();
-
+  // title = 'basic course 2023';
+  // parentName: string = 'hello, world!';
+  // user = {
+  //   name: 'Yura',
+  //   age: '27',
+  // };
+  // tooltip = 'Я подсказка для ссылки';
+  // inLineStyle = {
+  //   width: '50%',
+  //   background: 'green',
+  // };
+  // cssClass = 'blue';
+  // someNumber = 50;
+  // someDate = new Date();
   // lesson 06
-  ageOfUsers = [{ age: 20 }, { age: 26 }, { age: 22 }];
-
-  constructor() {
-    setTimeout(() => {
-      // this.ageOfUsers.push({ age: 16 });
-      this.ageOfUsers = [...this.ageOfUsers, { age: 16 }];
-    }, 3000);
-  }
-
+  // ageOfUsers = [{ age: 20 }, { age: 26 }, { age: 22 }];
+  // constructor() {
+  //   setTimeout(() => {
+  // this.ageOfUsers.push({ age: 16 });
+  //     this.ageOfUsers = [...this.ageOfUsers, { age: 16 }];
+  //   }, 3000);
+  // }
   // lesson  07
-  color = 'green';
+  // color = 'green';
+  // changeColor(newColor: string) {
+  //   this.color = newColor;
+  // }
+  // checkCurrentValue(event: Event) {
+  //   //! чтобы typescript не ругался на value
+  //   const target = event.target as HTMLInputElement;
+  //   console.log('event :>> ', target.value);
+  // }
+  // setNewName(newName: string) {
+  //   this.parentName = newName;
+  // }
+  // lesson  11
+  toggler: boolean = true;
+  str: string = '';
 
-  changeColor(newColor: string) {
-    this.color = newColor;
-  }
-
-  checkCurrentValue(event: Event) {
-    //! чтобы typescript не ругался на value
-    const target = event.target as HTMLInputElement;
-
-    console.log('event :>> ', target.value);
-  }
-
-  setNewName(newName: string) {
-    this.parentName = newName;
-  }
+  arr: any[] = ['text', 123, true, [1, 'abc'], NaN, Symbol];
 }
