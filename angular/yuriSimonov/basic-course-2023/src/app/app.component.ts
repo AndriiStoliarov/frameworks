345f0f48ed6dbe1b286ed6bb08c8de1fe3bcd05e
Andrii Stoliarov <andrey159852@gmail.com>
2025-12-18 17:25:06 +0200

    lesson 12 passed

--- angular/yuriSimonov/basic-course-2023/src/app/app.component.ts
+++ angular/yuriSimonov/basic-course-2023/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -50,4 +51,20 @@ export class AppComponent {
   str: string = '';
 
   arr: any[] = ['text', 123, true, [1, 'abc'], NaN, Symbol];
+
+  obj: any = {
+    first: {
+      second: {
+        third: {
+          age: 11,
+          name: 'Mike',
+        },
+      },
+    },
+  };
+
+  someTrackByMethod(index: number, item: any) {
+    console.log(1);
+    return item;
+  }
 }
