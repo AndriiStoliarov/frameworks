5990a68cb92b3b117ea56213a04b74422cd576d3
Andrii Stoliarov <andrey159852@gmail.com>
2025-12-30 18:25:12 +0200

    lesson 19 passed

--- angular/yuriSimonov/basic-course-2023/src/app/directives/custom-directive.directive.ts
+++ angular/yuriSimonov/basic-course-2023/src/app/directives/custom-directive.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appCustomDirective]',
 })
 export class CustomDirectiveDirective {
-  constructor() {
+  // класс ElementRef служит для типизации обычных html-элементов.
+  //! security risk
+  constructor(private element: ElementRef) {
     console.log('appCustomDirective');
+    console.log('element :>> ', this.element);
+
+    this.element.nativeElement.style.color = 'red';
   }
+
+  // @HostBinding('style.color') color: string = 'lime';
+  // @HostBinding('attr.color') attr: string = 'lime';
+  // @HostBinding('class.color') class: boolean = true;
+
+  @HostBinding('style.color') color = null;
+  @HostBinding('attr.color') attr = undefined;
+  @HostBinding('class.color') class = false;
 }
