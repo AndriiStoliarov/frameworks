70b94eedbc93167fb526c1f97a82493f315b5f3c
Andrii Stoliarov <andrey159852@gmail.com>
2026-01-14 20:20:55 +0200

    lesson 28 passed

--- angular/yuriSimonov/basic-course-2023/src/app/modules/first/first.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-  selector: 'app-first',
-  templateUrl: './first.component.html',
-  styleUrl: './first.component.scss',
-  // encapsulation: ViewEncapsulation.None,
-})
-export class FirstComponent {}
