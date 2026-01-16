60d53031bae41b9017189eb49adc345faf3ad0e4
Andrii Stoliarov <andrey159852@gmail.com>
2026-01-16 18:56:58 +0200

    lesson 29 passed

--- angular/yuriSimonov/basic-course-2023/src/app/app.component.ts
+++ angular/yuriSimonov/basic-course-2023/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChildComponent } from './components/child/child.component';
-import { Observable } from 'rxjs';
+import { filter, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +15,7 @@ export class AppComponent {
   //* Метод pipe() используется для задания различных манипуляций над исходными данными потока, перед тем как они попадут в метод subscribe();
   //* subscribe() используется для получения итоговых данных из потока/для подписки на происходящее событие;
 
-  someObservable = new Observable((sub) => {
+  someObservable$ = new Observable((sub) => {
     //! sub - подписчик, когда мы воспользуемся методом subscribe
     console.log(':)');
 
@@ -31,19 +31,27 @@ export class AppComponent {
     }, 2000);
   });
 
+  numberObservable$: Observable<number> = new Observable((sub) => {
+    sub.next(1);
+    sub.next(2);
+    sub.next(3);
+    sub.next(4);
+    sub.next(5);
+  });
+
   ngOnInit() {
-    this.someObservable.subscribe(console.log, console.log);
-    this.someObservable.subscribe(
+    this.someObservable$.subscribe(console.log, console.log);
+    this.someObservable$.subscribe(
       (value) => console.log('2 next: ', value),
       (error) => console.log('2 error: ', error),
       () => console.log('2 Поток успешно завершен')
     );
-    this.someObservable.subscribe({
+    this.someObservable$.subscribe({
       next: (value) => console.log('3 next: ', value),
       error: (error) => console.log('3 error: ', error),
       complete: () => console.log('3 Поток успешно завершен'),
     });
-    this.someObservable.subscribe({
+    this.someObservable$.subscribe({
       next(value) {
         console.log('4 next: ', value);
       },
@@ -54,5 +62,12 @@ export class AppComponent {
         console.log('4 Поток успешно завершен');
       },
     });
+
+    this.numberObservable$
+      .pipe(
+        map((item) => item * 10),
+        filter((item) => item < 25)
+      )
+      .subscribe(console.log);
   }
 }
