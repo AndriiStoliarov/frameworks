7650b93f4709cab8ef5f66399b6f90f27efb44cf
Andrii Stoliarov <andrey159852@gmail.com>
2026-02-20 21:31:15 +0200

    lesson 5 timepoint 07:50

--- angular/dinsTheProger/course-for-beginners-2024/src/app/components/my-calculator/my-calculator.component.ts
+++ angular/dinsTheProger/course-for-beginners-2024/src/app/components/my-calculator/my-calculator.component.ts
@@ -1,19 +1,59 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface CalcGroup {
+  first: CalcVar;
+  second: CalcVar;
+  operation: CalcOperations;
+}
+
+interface CalcVar {
+  value: number;
+  modificator: CalcModifiers;
+}
+
+enum CalcOperations {
+  plus = '+',
+  minus = '-',
+  multiply = '*',
+  divide = '/',
+}
+
+enum CalcModifiers {
+  none = 'none',
+  sin = 'sin',
+  cos = 'cos',
+  square = 'square',
+}
 
 @Component({
   selector: 'app-my-calculator',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './my-calculator.component.html',
   styleUrl: './my-calculator.component.scss',
 })
 export class MyCalculatorComponent {
-  public first: number = 1;
-  public second: number = 1;
-  public operation: string = '+';
-  public operations: string[] = ['+', '-', '*', '/'];
+  public calcOperations = CalcOperations;
+  public calcModifiers = CalcModifiers;
   // public result: number | undefined = undefined;
   public result?: number;
 
-  public calc() {}
+  public calc() {
+    switch (this.operation) {
+      case '+':
+        this.result = this.first + this.second;
+        break;
+      case '-':
+        this.result = this.first - this.second;
+        break;
+      case '*':
+        this.result = this.first * this.second;
+        break;
+      case '/':
+        this.result = this.first / this.second;
+        break;
+    }
+  }
 }
