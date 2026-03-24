36ab1cbf58a32b678c0cb38376bec7fca7f78926
Andrii Stoliarov <andrey159852@gmail.com>
2026-03-24 19:13:46 +0200

    main course timepoint 04:30:30

--- angular/ivanChernyakov/course-for-beginners-2024/src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
+++ angular/ivanChernyakov/course-for-beginners-2024/src/app/pages/settings-page/avatar-upload/avatar-upload.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { SvgIconComponent } from '../../../common-ui/svg-icon/svg-icon.component';
+import { DndDirective } from '../../../common-ui/directives/dnd.directive';
 
 @Component({
   selector: 'app-avatar-upload',
   standalone: true,
-  imports: [SvgIconComponent],
+  imports: [SvgIconComponent, DndDirective],
   templateUrl: './avatar-upload.component.html',
   styleUrl: './avatar-upload.component.scss',
 })
-export class AvatarUploadComponent {}
+export class AvatarUploadComponent {
+  preview = signal<string>('/assets/imgs/avatar-placeholder.png');
+
+  fileBrowserHandler(event: Event) {
+    const file: File | undefined = (event.target as HTMLInputElement)
+      ?.files?.[0];
+
+    if (!file || !file.type.match('image')) return;
+
+    const reader: FileReader = new FileReader();
+
+    reader.onload = (event) => {
+      this.preview.set(event.target?.result?.toString() ?? '');
+    };
+
+    reader.readAsDataURL(file);
+  }
+}
