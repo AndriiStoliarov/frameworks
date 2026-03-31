5b3da8edd90dce6c304350b4a4cb99d7e7d00e7e
Andrii Stoliarov <andrey159852@gmail.com>
2026-03-31 20:56:33 +0300

    main course timepoint 04:54:10

---
+++ angular/ivanChernyakov/course-for-beginners-2024/src/app/pages/search-page/profile-filters/profile-filters.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-profile-filters',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './profile-filters.component.html',
+  styleUrl: './profile-filters.component.scss',
+})
+export class ProfileFiltersComponent {
+  fb: FormBuilder = inject(FormBuilder);
+
+  searchForm = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    stack: [''],
+  });
+
+  constructor() {}
+}
