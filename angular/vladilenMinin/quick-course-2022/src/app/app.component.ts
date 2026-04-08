5dc190df3bf5eaa1086a7c34719e21fc8358b1db
Andrii Stoliarov <andrey159852@gmail.com>
2026-04-08 20:38:38 +0300

    main course timepoint 00:24:00

--- angular/vladilenMinin/quick-course-2022/src/app/app.component.ts
+++ angular/vladilenMinin/quick-course-2022/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { TitleCasePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductComponents } from './components/product/product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleCasePipe],
+  imports: [RouterOutlet, TitleCasePipe, ProductComponents],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'angular app';
--- angular/vladilenMinin/quick-course-2022/src/app/components/product/product.component.ts
+++ angular/vladilenMinin/quick-course-2022/src/app/components/product/product.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
+  standalone: true,
 })
 export class ProductComponents {}
