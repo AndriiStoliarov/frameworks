c55d6c49dc0f47ea3653840c70d15841481a58bb
Andrii Stoliarov <andrey159852@gmail.com>
2026-04-15 18:33:52 +0300

    main course timepoint 01:33:50

--- angular/vladilenMinin/quick-course-2022/src/app/components/create-product/create-product.component.ts
+++ angular/vladilenMinin/quick-course-2022/src/app/components/create-product/create-product.component.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './create-product.component.html',
   styleUrl: './create-product.component.scss',
 })
 export class CreateProductComponent {
   form = new FormGroup({
-    title: new FormControl<string>(''),
+    title: new FormControl<string>('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
   });
+
+  get title() {
+    return this.form.controls.title as FormControl;
+  }
 
   submit() {
     console.log(this.form.value);
